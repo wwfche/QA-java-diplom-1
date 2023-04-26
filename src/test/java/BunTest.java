b4daad91cabdfa2073a8f082c87b06f209e02bc2
wwfche <khutsiev.d@gmail.com>
2023-04-26 10:53:40 +0300

    fix rev

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -19,13 +19,18 @@ public class BunTest {
         this.expected2 = expected2;
     }
 
+
     @Parameterized.Parameters(name = "{index}: sort[{0}]={1}")
     public static Object[][] parameterizedTest() {
-        return new Object[][] {
-                {"Черная булка", 50f, "Черная булка", 50f},
-                {"Белая булка", 75f,"Белая булка", 75f}
-        };
-    }
+        return new Object[][]{
+                {"Черная булка", 51f},
+                {"", Float.MAX_VALUE},
+                {"0", Float.MIN_VALUE},
+                {"()&*^%$#@!_", 0f},
+                {"0987654321", 345323.533f},
+                {"null", -99999f}
+    };
+}
 
     @Test
     public void getNameReturnsNameOfBun() {
